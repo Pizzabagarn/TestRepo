8c320e7c42044d297fb8e8a8d1249e3f515d83c9
AlexanderWestman <superalle96@gmail.com>
2025-03-28 09:56:10 +0100

    Gjorde en alex metod

--- Main.java
+++ Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("What is your name?");
         String name = input.nextLine();
         System.out.println("My name is " + name);
+        Alex alex = new Alex();
+        alex.sayHey();
     }
 }
